44090e382f27bd7651031052f71ef5b0e2c4fdbe
junior <lewisomogi01@gmail.com>
2024-03-02 23:19:18 -0800

    rest api

--- vehicle_api/views.py
+++ vehicle_api/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from vehicle_api.models import vehicle
 
 # Create your views here.
+@api_view (['GET'])
+def vehicle_list(requests):
+    vehicles = vehicle.objects.all()
+    serializer = VehicleSerializer(vehicles, many= True)
+    return Response(serializer.data) 

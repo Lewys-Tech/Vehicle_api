c4c8159cecf0a8ba35c5a3d1a3fa73eb22c7dd79
junior <lewisomogi01@gmail.com>
2024-03-06 07:42:14 -0800

    api commit

--- vehicle_api/models.py
+++ vehicle_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Vehicle(models.Model):
     vehicle_type = models.CharField(max_length=200)
     vehicle_regNo = models.IntegerField()
-    vehicle_owner = models.CharField()
+    vehicle_owner = models.CharField(max_length=100)
     
     
     
--- vehicle_api/views.py
+++ vehicle_api/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from vehicle_api.models import vehicle
+from vehicle_api.models import Vehicle
+from vehicle_api.serializer import VehicleSerializer
 
-# Create your views here.
-@api_view (['GET'])
-def vehicle_list(requests):
-    vehicles = vehicle.objects.all()
-    serializer = VehicleSerializer(vehicles, many= True)
-    return Response(serializer.data) 
+@api_view(['GET'])
+def vehicle_list(request):
+    vehicles = Vehicle.objects.all()  # Corrected variable name
+    serializer = VehicleSerializer(vehicles, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def vehicle_create(request):
+    serializer = VehicleSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(serializer.errors)
